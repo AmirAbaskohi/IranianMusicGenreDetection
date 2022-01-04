813ec608987874b01e62689507d47015843e8014
AmirHosseinMissMe <amirhossein2324236@gmail.com>
2022-01-04 14:14:55 +0330

    Added music splitting in preprocess.

--- Preprocess/preprocess.py
+++ Preprocess/preprocess.py
@@ -1,11 +1,14 @@
 import os
 import numpy as np
+from numpy.lib.utils import source
 import pandas as pd
 import matplotlib.pyplot as plt
 from tqdm import tqdm
 from mutagen.mp3 import MP3
+from pydub import AudioSegment
 
 DATA_PATH = ".." + os.sep + "data"
+FINAL_DATA_PATH = ".." + os.sep + "final_data"
 
 size = 25
 params = {'legend.fontsize': size,
@@ -17,12 +20,16 @@ params = {'legend.fontsize': size,
           'axes.titlepad': 25}
 plt.rcParams.update(params)
 
+def createDirectoryIfDoesNotExists(path):
+    if not os.path.exists(path):
+        os.makedirs(path)
+
 def getAllMusicsTimeInSeconds():
     result = []
     for subdir, dirs, files in os.walk(DATA_PATH):
         for file in tqdm(files):
-            file_path = DATA_PATH + os.sep + subdir.split(os.sep)[2] + os.sep + file 
-            audio = MP3(file_path)
+            filePath = DATA_PATH + os.sep + subdir.split(os.sep)[2] + os.sep + file 
+            audio = MP3(filePath)
             result.append(int(audio.info.length))
     return result
 
@@ -30,7 +37,7 @@ def calLossForSplitDuration(durations, splitDuration):
     result = np.array(durations)
     return result%splitDuration
 
-if __name__ == '__main__':
+def getReportOfDataSet():
     musicDurationsInSeconds = getAllMusicsTimeInSeconds()
     allDuration = sum(musicDurationsInSeconds)
     result = []
@@ -45,3 +52,33 @@ if __name__ == '__main__':
     plt.title('Data split result')
     plt.xlabel('Split duration')
     plt.savefig('split_result', dpi=600)
+
+def splitMusics(lengthInMilliseconds):
+    result = []
+    g = 0
+    for subdir, dirs, files in os.walk(DATA_PATH):
+        for file in tqdm(files):
+            splittedFile = file.split('.')
+            fileName = ".".join(splittedFile[:-1])
+            directory = subdir.split(os.sep)[2]
+            filePath = DATA_PATH + os.sep + directory + os.sep + file
+
+            sound = AudioSegment.from_file(filePath)
+            soundLength = len(sound)
+            numberOfParts = soundLength // lengthInMilliseconds
+            loss = soundLength - numberOfParts * lengthInMilliseconds
+            skipEnd = loss // 2
+            skipStart = loss - skipEnd
+
+            start = skipStart + 1
+            for i in range(numberOfParts):
+                partFilePath = FINAL_DATA_PATH + os.sep + directory + os.sep + fileName + "_" + str(i+1) + ".wav"
+                part = sound[start: start+lengthInMilliseconds+1]
+
+                createDirectoryIfDoesNotExists(FINAL_DATA_PATH + os.sep + directory)
+
+                part.export(partFilePath , format="wav")
+                start = start+lengthInMilliseconds+1
+
+if __name__ == '__main__':
+    splitMusics(30000)
